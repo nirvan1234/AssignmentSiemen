0b6d198de80b72157edf9cc8320bcd1cd01d78ea
Nirvan Pandagre <nirvanpandagre@Nirvans-MacBook-Air.local>
2024-02-08 20:32:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React ,{useState, useEffect} from 'react';
 
 function App() {
+  const [isPaused, setIsPaused] = useState(false);
+  const [isActive, setIsActive] = useState(true);
+  const [time, setTime] = useState(0);
+
+   useEffect(() =>{
+   if(isPaused && time !== 12000){
+    setTimeout(() =>{
+      setTime(time +1 );
+    }, 1000 )
+   }else{
+      StopCondition();
+   }
+   },[time, isPaused,isActive])
+
+   console.log(time, isPaused,isActive)
+
+
+   function StopCondition(){
+    setTime(time);
+    setIsActive(false);
+   }
+
+   function startWatch(){
+    setIsPaused(true);
+    setIsActive(true);
+    setTime((prevState) => ( prevState+ 1 ))
+   }
+
+   function stopWatch(){
+    setIsPaused(false);
+   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{time}</h1>
+     <button onClick={() => startWatch()}>
+     <h4>Start</h4>
+     </button>
+     <button onClick={() => stopWatch()}>
+     <h4>Stop</h4>
+      </button>
+
+     
     </div>
   );
 }
